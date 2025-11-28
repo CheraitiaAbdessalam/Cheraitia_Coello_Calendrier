b4bfa96687b1066b5e749ae2a6327480b36969c0
esteban coellofernandez <esteban.coellofernandez@saintmichelannecy.fr>
2025-11-28 12:01:44 +0100

    v1

---
+++ Cheraitia_Coello_Calendrier/Cartes/CarteJour1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cheraitia_Coello_Calendrier.Cartes
+{
+    /// <summary>
+    /// Logique d'interaction pour CarteJour1.xaml
+    /// </summary>
+    public partial class CarteJour1 : UserControl
+    {
+        public CarteJour1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Cheraitia_Coello_Calendrier/MenuCarte/MenuCartes.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cheraitia_Coello_Calendrier.MenuCarte
+{
+    /// <summary>
+    /// Logique d'interaction pour MenuCartes.xaml
+    /// </summary>
+    public partial class MenuCartes : UserControl
+    {
+        public ObservableCollection<string> Cartes { get; set; }
+
+        public MenuCartes()
+        {
+        InitializeComponent();
+            Cartes = new ObservableCollection<string>
+            {
+            };
+
+            CardsListBox.ItemsSource = Cartes;
+
+        }
+        public class Carte
+        {
+            public string ImagePath { get; set; }
+            public string Titre { get; set; }
+        }
+    }
+}
